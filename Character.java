63b3477a1dabb6d6e4ef61fe837de8fc04470919
jasminesaligan <110054198+jasminesaligan@users.noreply.github.com>
2022-08-22 11:08:16 +0800

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,22 @@
+public class Character {
+    int strength;
+    int agility;
+    int intelligence;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("Hello I am " + name);
+    }
+
+    public void sayMyStrength() {
+        System.out.println("Watchout! my strength is " + strength);
+    }
+    
+    public void sayMyAgility() {
+        System.out.println("Prepare! my agility is " + agility);
+    }
+
+    public void sayMyIntelligence() {
+        System.out.println("Get Ready! my intelligence is " + intelligence + "\n");
+    }
+} 
