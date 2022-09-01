7acb302a40d4a8a06ec479c36c7127682e042c7f
jasminesaligan <110054198+jasminesaligan@users.noreply.github.com>
2022-09-01 10:06:09 +0800

    Add files via upload

---
+++ Game/Gear.java
@@ -0,0 +1,15 @@
+public class Gear {
+    // Gear attributes
+    String name;
+    int defense;
+    float weight;
+    String rarity;
+    boolean hasElementalResistance;
+
+    // Gear method
+
+    public void showGearDescription() {
+        System.out.println ("An invincible shield owned by Jasmine");
+    }
+
+}
