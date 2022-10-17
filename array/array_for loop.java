c4dfad402a80618107fb64b9ccb2d293146e562c
jasminesaligan <110054198+jasminesaligan@users.noreply.github.com>
2022-10-17 10:23:22 +0800

    Create array_for loop.java

---
+++ array/array_for loop.java
@@ -0,0 +1,19 @@
+public class Main
+{
+    public static void main(String[] args) {
+      char[] name = {'J', 'A', 'S', 'M', 'I', 'N', 'E'};
+    //   System.out.println(name[1]);
+    //   System.out.println(name[6]);
+    //   System.out.println(name[0]);
+    // for 
+      for (int i = 0; i < name.length; i++) {
+       if (name[i] == 'A' || name[i] == 'I' || name[i] == 'E') {
+				// Display each vowel
+				System.out.println(name[i]); 
+             
+       }
+        //   System.out.println(name[i]);
+      }
+    
+    }
+}
