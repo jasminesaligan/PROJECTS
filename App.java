33bdcb9a2f2ee067a993b5060307e13fa03110c4
Jasmine Saligan <110054198+jasminesaligan@users.noreply.github.com>
2022-11-28 16:58:50 +0800

    Add files via upload
    Machine Problem: 3 Compare Max

--- App.java
+++ App.java
@@ -1,51 +1,33 @@
+import java.util.Scanner; 
+
 public class App {
-    public static void main(String[] args) {
-        
-        Character knight = new Character(strength: 10, agility: 5, intelligence: 0, name:"Chen");
+    public static void main(String[] args) throws Exception {
+       
+       
+       
+        Scanner scanner = new Scanner(System.in);
 
-        knight.strength = 10;
-        knight.agility = 5;
-        knight.intelligence = 0;
-        knight.name = "Chen";
-        knight.sayMyStrength();
-        knight.sayMyAgility();
-        knight.sayMyIntelligence();
-        knight.sayMyName();
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int num3 = scanner.nextInt();
+
+        if (num1 > num2 && num1 > num3)
+         {
+            System.out.print("The maximum number is: " + num1);
+        } 
+        else if (num2 > num3)
+        {
+            System.out.println("The maximu number is: " + num2);
+        }
+        else {
+            System.out.println("The maximum number is: " + num3);
+        }
 
 
-        Character mage = new Character(strength: 0, agility: 5, intelligence: 10, name:"Akasha");
-
-        mage.strength = 0;
-        mage.agility = 5;
-        mage.intelligence = 10;
-        mage.name = "Akasha";
-        mage.sayMyStrength();
-        mage.sayMyAgility();
-        mage.sayMyIntelligence();
-        mage.sayMyName();
-
-
-        Character thief = new Character(strength: 5, agility: 10, intelligence: 0, name:"Gondar");
-
-        thief.strength = 5;
-        thief.agility = 10;
-        thief.intelligence = 0;
-        thief.name = "Gondar";
-        thief.sayMyStrength();
-        thief.sayMyAgility();
-        thief.sayMyIntelligence();
-        thief.sayMyName();
-
-        Character archer = new Character(strength: 5, agility: 5, intelligence: 5, name:"Jasmine");
-
-        archer.strength = 5;
-        archer.agility = 5;
-        archer.intelligence = 5;
-        archer.name = "Jasmine";
-        archer.sayMyStrength();
-        archer.sayMyAgility();
-        archer.sayMyIntelligence();
-        archer.sayMyName();
-        
     }
 }
