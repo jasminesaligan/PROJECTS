2d96d868e877dce435b54b141039c1174b10d1e6
Jasmine Saligan <110054198+jasminesaligan@users.noreply.github.com>
2022-12-11 22:42:39 +0800

    Add files via upload

---
+++ LOOP/LOOP_WHILE_FOR_DOWHILE.java
@@ -0,0 +1,25 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+       // start = 4
+       // end = 25
+       // interval = 5
+       
+        // int i = 1;
+        // do {
+        //     System.out.println(i);
+        //     i = i + 4;
+        // }
+        // while (i <=5);
+
+        // int i = 10;
+        // while (i >= 1); {
+        //     System.out.println(i);
+        //     i-=2;
+        // }
+
+        for (int i = 10; i >= 1; i-=2) {
+            System.out.println(i);
+        }
+    }
+}
